6bf6f4724009d5c87efeb6a4cc17aad7fdd97549
Rushi <rushikeshunde999@gmail.com>
2024-06-03 22:40:35 +0530

    intial commit

---
+++ MultiProgramming-Operating-System-Simulation-master/Phase_2/src/main.cpp
@@ -0,0 +1,16 @@
+#include "Core_Function.cpp"
+
+int main()
+{
+    string in = "..\\Input\\custom_test_input.txt";
+    string out = "..\\Output\\test_output.txt";
+
+    // string in = "..\\Input\\phase2_input.txt";
+    // string out = "..\\Output\\phase2_output.txt";
+
+    OS NewOS(in,out);
+    NewOS.LOAD();
+    
+    return 0;
+}
+
